d4202d9535c074fb80485c8b2224d63c486e09c3
Carl <carl.bachmann@gmx.de>
2017-12-01 16:54:34 +0100

    Korrekturen und Kosmetische Änderungen
    Methoden- und Klassennamen vereinheitlicht
    
    Unnötige Kommentare und ungenutzten Code entfernt

--- src/Client.java
+++ src/Client.java
@@ -1,32 +1,22 @@
 import java.net.*;
 import java.io.*;
-import java.util.*;
-
-import java.util.HashMap;
-
 
 public class Client {
-	Socket server;
-
 	public static void main(String args[]) {
-		try {
-			Socket server = new Socket("localhost", 2345);
-			
-			// in
+        try {
+            Socket server = new Socket("localhost", 2345);
+
 			InputStream inputStream = server.getInputStream();
 			BufferedReader bufferedReaderInputStream = new BufferedReader(new InputStreamReader(inputStream));
-			
-			// out
+
 			OutputStream outputStream = server.getOutputStream();
 			PrintWriter printWriterOutputStream = new PrintWriter(outputStream, true);
-			
-			// damit gleichzeitig gesendet und empfangen werden kann hat jeder Client zwei eigene Threads
-			// startet sendenThread und empfangenThread
-			sendenThread sendet = new sendenThread(printWriterOutputStream); 
+
+			sendThread sendet = new sendThread(printWriterOutputStream); 
 			sendet.start();
-			empfangenThread empfaengt = new empfangenThread(bufferedReaderInputStream, server); 
+
+			receiveThread empfaengt = new receiveThread(bufferedReaderInputStream);
 			empfaengt.start();
-			
 		} catch(UnknownHostException e) {
 			System.out.println("Can't find host.");
 		} catch (IOException e) {
@@ -35,19 +25,14 @@ public class Client {
 	}	
 }
 
-// ein Thread dient dem Senden
-class sendenThread extends Thread {
-	// der printWriterOutputStream muss übergeben werden
-	PrintWriter pout;
+class sendThread extends Thread {
+	private PrintWriter pout;
 	
-	sendenThread(PrintWriter pout) {
+	sendThread(PrintWriter pout) {
 		this.pout = pout;
 	}
 
-	// der Client kann Nachrichten über den printWriterOutputStream senden
-	// dieser muss jedoch durch flush() sofort geleert werden, damit nicht erst eine große
-	// Nachrichtenansammlung geschickt wird
-	void senden(String message, PrintWriter printWriterOutputStream) {
+	private void send(String message, PrintWriter printWriterOutputStream) {
 		printWriterOutputStream.println(message);
 		printWriterOutputStream.flush();
 	}
@@ -55,41 +40,31 @@ class sendenThread extends Thread {
 	public void run(){
 		try {
 			while(true) {
-				// fängt den Stream der Tastatur ab
 				InputStreamReader tastatur = new InputStreamReader(System.in);
 				BufferedReader bufTastatur = new BufferedReader(tastatur);
-		
-				// speichert Daten in der Variable tastatureingabe und sendet diese
+
 				String tastatureingabe = bufTastatur.readLine();
-				
-				// überprüft vor Senden der Nachricht an den Server, ob 'abmelden' gewünscht war
-				// falls ja, terminiere den Client
+
 				if (tastatureingabe.equals("/abmelden")) {
 					System.exit(0);
-				}
-				
-				// ansonsten wird die eingegebene Nachricht über den printWriterOutputStream
-				// über die Methode senden() an den Server gesendet
-				senden(tastatureingabe, pout);
+				} else {
+                    send(tastatureingabe, pout);
+                }
 			}			
-		} catch (IOException e) { System.out.println("Der sendenThread funktioniert nicht mehr."); }		
+		} catch (IOException e) {
+		    System.out.println("Der sendThread funktioniert nicht mehr.");
+		}
 	}
 }
 
-// ein Thread dient dem Empfangen
-class empfangenThread extends Thread {
-	// dazu muss der Socket und der BufferedReader übergeben werden
-	BufferedReader bis;
-	Socket server;
-	
-	empfangenThread(BufferedReader bis, Socket server) {
+class receiveThread extends Thread {
+	private BufferedReader bis;
+
+	receiveThread(BufferedReader bis) {
 		this.bis = bis;
-		this.server = server;
 	}
 
-	// Nachrichten können vom Server entgegengenommen werden
-	// falls sie nicht angenommen werden kann, wird eine Fehlermeldung mit Fehlerursache ausgegeben
-	String annehmen(BufferedReader bufferedReaderInputStream) {
+	private String receive(BufferedReader bufferedReaderInputStream) {
 		try {
 			return bufferedReaderInputStream.readLine();
 		} catch (IOException e) {
@@ -100,10 +75,8 @@ class empfangenThread extends Thread {
 	}
 	
 	public void run(){
-		// empfängt die Nachrichten vom Server über die Methode annehmen()
-		// gibt diese aus, solange der Server läuft
 		while(true) {
-			String ankommendeNachricht = annehmen(bis);
+			String ankommendeNachricht = receive(bis);
 			if (ankommendeNachricht != null) {
 				System.out.println(ankommendeNachricht);	
 			} else {
--- src/Server.java
+++ src/Server.java
@@ -3,39 +3,27 @@ import java.net.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
 public class Server {
-	// der Port ist in diesem Fall 2345
-	private int port;
-	// ArrayList zur Speicherung der aktuell angemeldeten Nutzer
-	private ArrayList<clientThread> threads = new ArrayList<clientThread>();
-	// HashMap zur Speicherung der angelegten Nutzernamen und dazugehörigen Passwörter
-	private HashMap<String, String> hmap = new HashMap<String, String>();
-	// Anlegen des Serversockets server
+	private int port; // in diesem Fall 2345
 	private ServerSocket serverSocket;
 	
 	private Server(int port) {
 		this.port = port;
 	}
-	public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException {
 		System.out.println("Server hat gestartet \nZum Beenden '/stop' eingeben.");
 		Server server = new Server(2345);
 		server.serverSocket = new ServerSocket(server.port);
 		Verwaltung verwaltung = new Verwaltung();
 		
 		while (true)  {
-			ReadingServerInput eingabe = new ReadingServerInput(server);
+			ReadingServerInput eingabe = new ReadingServerInput();
 			eingabe.start();
 			
 			clientThread clientThread = new clientThread(server.serverSocket.accept(), verwaltung.threads, verwaltung.hmap);
 			clientThread.start();
 		}
-	}
-	
-	public void end() {
-		try {
-			serverSocket.close();
-		} catch (IOException e) {}
 	}
 }
 
@@ -44,53 +32,46 @@ class clientThread extends Thread {
 	private ArrayList<clientThread> threads;
 	private HashMap<String, String> hmap;
 	private String name;
-	private String passwort;
-	
-	clientThread(Socket client, ArrayList<clientThread> threads, HashMap<String, String> hmap) { 
+
+    clientThread(Socket client, ArrayList<clientThread> threads, HashMap<String, String> hmap) {
 		this.client = client; 
 		this.threads = threads;
 		this.hmap = hmap;
 	}
-	
-	private  void send(String message) {
+
+	private void send(String message) {
 		try {
 			DataOutputStream dataOutputStream = new DataOutputStream(client.getOutputStream());
 			PrintWriter printWriterOutputStream = new PrintWriter(dataOutputStream, true);
 			
 			printWriterOutputStream.println(message);
 			printWriterOutputStream.flush();
-		} catch (IOException e) { System.out.println("Fehler beim Senden"); }
-	}
-	
-	
-	private String accept(BufferedReader bufferedReaderInputStream) {
-		try { 
-			String x = bufferedReaderInputStream.readLine();
-			// System.out.println("Ich habe empfangen: " + x);
-			return x; 
 		} catch (IOException e) {
-			System.out.println("Error connectng to host.");
+		    System.out.println("Fehler beim Senden");
+		}
+	}
+
+	private String receive(BufferedReader bufferedReaderInputStream) {
+		try {
+			return bufferedReaderInputStream.readLine();
+		} catch (IOException e) {
+			System.out.println("Fehler beim Empfangen der Nachricht.");
 			e.printStackTrace();
 			return null;
 		}
 	}
-	
-	
-	public void run(){ 
-		// Bearbeitung einer aufgebauten Verbindung
+
+	public void run(){
 		try {
 			InputStream inputStream = client.getInputStream();
-			OutputStream outputStream = client.getOutputStream();
 			
 			BufferedReader bufferedReaderInputStream = new BufferedReader(new InputStreamReader(inputStream));
 						
 			while(true) {
-				send("Name: "); // null sendet an den Client
-				String name = accept(bufferedReaderInputStream);
-				this.name = name;
+				send("Name: ");
+                name = receive(bufferedReaderInputStream);
 				send("Passwort: ");
-				String passwort = accept(bufferedReaderInputStream);
-				this.passwort = passwort;
+                String passwort = receive(bufferedReaderInputStream);
 				
 				if (!hmap.containsKey(name)) {
 					hmap.put(name, passwort);
@@ -108,76 +89,77 @@ class clientThread extends Thread {
 			threads.add(this);
 			
 			send("\nAktuelle Nutzer:");
-			String onlineListe = "";
-			for (int p = 0; p < threads.size(); p++) {
-				onlineListe = onlineListe + threads.get(p).name + "\n";
-			}
-			send(onlineListe);
-			
-			for (int z = 0; z < threads.size(); z++) {
-				if (!threads.get(z).name.equals(name)) {
-					threads.get(z).send(name + " hat sich eingeloggt");
-				}
-			}			
+			StringBuilder onlineListe = new StringBuilder();
+            for (clientThread thread1 : threads) {
+                onlineListe.append(thread1.name).append("\n");
+            }
+			send(onlineListe.toString());
+
+            for (clientThread thread : threads) {
+                if (!thread.name.equals(name)) {
+                    thread.send(name + " hat sich eingeloggt");
+                }
+            }
 			
 			while(true) {
-				String aString = accept(bufferedReaderInputStream);
+				String aString = receive(bufferedReaderInputStream);
 				if (aString != null) {
 					System.out.println(name + ": \t" + aString);
-					for(int i1 = 0; i1< threads.size(); i1++) {
-						if(threads.get(i1).name != name) {
-							threads.get(i1).send(name + ": \t" + aString);
-						}
-					}
+                    for (clientThread thread : threads) {
+                        if (thread.name.equals(name)) {
+                            thread.send(name + ": \t" + aString);
+                        }
+                    }
 				} else {
 					System.out.println(name + " hat seine Verbindung abgebrochen");
-					for(int i1 = 0; i1< threads.size(); i1++) {
-						if(threads.get(i1).name != name) {
-							threads.get(i1).send("Zu " + name + " besteht keine Verbindung mehr.");
-						}
-					}
+                    for (clientThread thread : threads) {
+                        if (thread.name.equals(name)) {
+                            thread.send("Zu " + name + " besteht keine Verbindung mehr.");
+                        }
+                    }
 					break;
 				}
 				
 			}
-		} catch ( IOException e ) {} // Fehler bei Ein- und Ausgabe
+		} catch ( IOException e ) {
+            System.out.println("Fehler bei Ein- und Ausgabe");
+        }
 		finally { 
 			if ( client != null ) try { 
 				client.close(); 
 				threads.remove(this);
-			} catch ( IOException e ) { } } 
+			} catch ( IOException e ) {
+                System.out.println("Fehler bei Ein- und Ausgabe");
+            }
+		}
 	}
 }
 
 class ReadingServerInput extends Thread {
-	
-	private Server server;
-	
-	public ReadingServerInput(Server server){
-		this.server = server;
-	}
+    ReadingServerInput(){}
+
 	public void run() {
 		try {
-		while(true) {
-			// fängt den Stream der Tastatur ab
-			InputStreamReader serverEingabe = new InputStreamReader(System.in);
-			BufferedReader bufTastatur = new BufferedReader(serverEingabe);
-					
-			// speichert Daten in der Variable tastatureingabe und sendet diese
-			String tastatureingabe = bufTastatur.readLine();
-			if (tastatureingabe.equals("/stop")) {
-				System.exit(0);
-			}
+		    while(true) {
+                // fängt den Stream der Tastatur ab
+                InputStreamReader serverEingabe = new InputStreamReader(System.in);
+                BufferedReader bufTastatur = new BufferedReader(serverEingabe);
+
+                // speichert Daten in der Variable tastatureingabe und sendet diese
+                String tastatureingabe = bufTastatur.readLine();
+                if (tastatureingabe.equals("/stop")) {
+                    System.exit(0);
+                }
+		    }
+	    } catch (IOException e) {
+		    System.out.println("Fail");
 		}
-	} catch (IOException e) { System.out.println("fail"); }
 	}
 }
 	
 class Verwaltung extends Thread {
-	// ArrayList zur Speicherung der aktuell angemeldeten Nutzer
-	protected ArrayList<clientThread> threads = new ArrayList<clientThread>();
-	// HashMap zur Speicherung der angelegten Nutzernamen und dazugehörigen Passwörter
-	protected HashMap<String, String> hmap = new HashMap<String, String>();
+    ArrayList<clientThread> threads = new ArrayList<>(); // angemeldete Nutzer
+    HashMap<String, String> hmap = new HashMap<>(); // Namen und PWer
 
-	Verwaltung(){ }
+	Verwaltung(){}
 }
